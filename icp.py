09a4c9c3de8da53182b74522825fcc99f7842c63
Ishaan Shah <ishaan.n.shah@gmail.com>
2022-09-18 02:41:42 +0530

    Improve stopping condition

--- icp.py
+++ icp.py
@@ -10,12 +10,12 @@ spot = exchange.load.load("./spot/pcd_1k.xyz")
 src = np.asarray(spot.vertices)
 src_pcd = ps.register_point_cloud("source", src)
 
-rot = Rotation.from_euler("XYZ", [5, 0, 0], degrees=True).as_matrix()
+rot = Rotation.from_euler("XYZ", [30, 0, 0], degrees=True).as_matrix()
 
-trg = src @ rot + np.ones(3)
+trg = src @ rot + np.random.rand()
 trg_pcd = ps.register_point_cloud("target", trg)
 
-for i in range(100):
+for i in range(500):
     s_m, t_m = src.mean(axis=0), trg.mean(axis=0)
     s_mc, t_mc = src - s_m, trg - t_m
     corr_idx = np.argmin(cdist(s_mc, t_mc), axis=1)
@@ -26,13 +26,12 @@ for i in range(100):
     if not np.isclose(np.linalg.det(r), 1):
         r = u @ np.diag([1, 1, -1]) @ vt
 
-    t = t_m - s_m @ rot
-    src = src @ rot + t
-    if np.linalg.norm(src - trg) < 1e-5:
+    t = t_m - (rot @ s_m.T).T
+    src = (rot @ src.T).T + t
+    if np.isclose(src, trg).all():
         break
     print(np.mean(np.linalg.norm(src - trg)))
     
-print(np.isclose(src, trg).all())
 pred_pcd = ps.register_point_cloud("predicted", src)
 
 ps.show()
